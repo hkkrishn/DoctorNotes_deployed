bfd550308a71d5314b4e239f0f74c04f675ffb13
Harikrishnan <hkkrishn563@gmail.com>
2019-03-05 15:39:59 -0800

    fixed uri

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV === 'production'){
-  module.exports = {mongoURI:'mongodb://hkkrishn:Hophop12@cluster0-pu2ry.mongodb.net/test?retryWrites=true'}
+  module.exports = {mongoURI:'mongodb+srv://hkkrishn:Hophop12@cluster0-pu2ry.mongodb.net/test?retryWrites=true'}
 } else{
   module.exports = {mongoURI:'mongodb://localhost/DoctorNotes-dev'}
 }
